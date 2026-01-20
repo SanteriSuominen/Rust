0d9f0d8287b56d0fa42e6394646c3431f761bba6
Santeri <santeri.suominen@kolumbus.fi>
2026-01-20 15:59:02 +0200

    Made direcotry hierarcy

---
+++ KT2/events/src/main.rs
@@ -0,0 +1,31 @@
+fn main() 
+{
+    let events = [
+        (1996_01_23, "JDK 1.0 released"),
+        (2008_12_03, "Python 3.0 released"),
+        (2015_05_15, "Rust 1.0.0 released"),
+        (2025_09_16, "Java 25 released"),
+        (2025_10_07, "python 3.14 released"),
+        (2025_12_11, "Rust 3.92.0 released"),
+    ];
+    
+    //println!("{} events", events.len());
+    //println!("{:#?}", events);
+
+    //let mut index = 0;
+
+    for (index, event) in events.iter().enumerate()
+    {
+        println!("[{}] {}: {}", index + 1, event.0, event.1);
+    }
+
+    for event in events {
+        println!("{}: {}", event.0, event.1)
+    }
+    
+    while index < events.len()
+    {
+        println!("[{}] {}: {}", index + 1, events[index].0, events[index].1);
+        index += 1;
+    }
+}
---
+++ KT2/example/src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() 
+{
+    let mut number: i32 = 5;
+    let mut word = "5";
+
+    println!("{}", number);
+    println!("{}", word);
+}
+
