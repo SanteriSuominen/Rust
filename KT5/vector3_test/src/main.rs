9386574de73798d18ab576496245f5276d085628
Santeri <santeri.suominen@kolumbus.fi>
2026-03-03 10:09:25 +0200

    sync

--- KT5/vector3_test/src/main.rs
+++ KT5/vector3_test/src/main.rs
@@ -1,54 +1,97 @@
-
-struct Vector3 
+use rand::RngExt;
+use std::thread::sleep;
+use std::time::Duration;
+struct Vector2 
 {
-    x: f64,
-    y: f64,
-    z: f64,
+    x: u32,
+    y: u32,
 }
 
 fn main() 
 {
-    let mut player_position = Vector3 
+    let mut player_position = Vector2
     {
-        x: 2.0,
-        y: 3.0,
-        z: 2.0,
+        x: 0,
+        y: 0,
     };
 
-    let target = Vector3
+    let mut target = Vector2
     {
-        x: 14.0,
-        y: 0.0,
-        z: 22.0,
+        x: 19,
+        y: 9,
     };
 
-    let mut distance = calculate_distance(player_position, target);
-
-    while distance > 0.0
+    let grid = Vector2
     {
-       distance = calculate_distance(player_position, target);
-       println!("Distance between player and target is {distance}");
+        x: 20,
+        y: 10,
+    };
+        update_map(&player_position, &target, &grid);
+        sleep(Duration::from_millis(100));
+    while true
+    {
+        while player_position.x != target.x || player_position.y != target.y
+        {
+            if player_position.x != target.x
+            {
+                if player_position.x < target.x
+                {
+                    player_position.x = player_position.x + 1;
+                }
 
-       if player_position.x > 0.0
-       {
-            player_position.x = player_position.x - 1.0;
-       }
-       if player_position.y > 0.0
-       {
-            player_position.y = player_position.y - 1.0;
-       }
-       if player_position.z > 0.0
-       {
-            player_position.z = player_position.z - 1.0;
-       }
+                else if player_position.x > target.x
+                {
+                    player_position.x = player_position.x - 1;
+                }   
+            }
+
+            if player_position.y != target.y
+            {
+                if player_position.y < target.y
+                {
+                    player_position.y = player_position.y + 1;
+                }
+
+                else if player_position.y > target.y
+                {
+                    player_position.y = player_position.y - 1;
+                }
+            } 
+            update_map(&player_position, &target, &grid);
+            sleep(Duration::from_millis(100));
+        }
+        println!("Hit");
+        let mut rng = rand::rng();
+        target.x = rng.random_range(0..=19);
+        target.y = rng.random_range(0..=9);
     }
 }
 
-fn calculate_distance(player_position: Vector3, target: Vector3) -> f64
+fn update_map(player_position: &Vector2, target: &Vector2, grid: &Vector2)
 {
-    let distance = ((player_position.x - target.x).powi(2)
-                   +(player_position.y - target.y).powi(2)
-                   +(player_position.z - target.z).powi(2)).sqrt();
-    
-    distance
+    print!("\x1B[2J\x1B[H");
+    let grid_y = grid.y;
+    let grid_x = grid.x;
+
+    for y in 0..grid_y
+    {
+        for x in 0..grid_x
+        {
+            if player_position.x == x && player_position.y == y
+            {
+                print!("*");
+            }
+
+            else if target.x == x && target.y == y
+            {
+                print!("o");
+            }
+
+            else 
+            {
+                print!(".");    
+            }
+        }
+        println!("");
+    }
 }
